e399f7378861d916f12127a33ccc9b64abaf9f4c
Rivaldo2117051054 <rivaldorr547@gmail.com>
2021-11-24 12:43:21 +0700

    Membuat piramid

---
+++ piramid bintang.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	for(int a=0; a<5; a++){
+		//jika a lebih besar dari 5 akan melakukan perulangan
+		//ditambah sampai false
+		for(int b=0; b<a+1; b++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	for(int c=5; c>0; c--){
+			//jika c lebih besar dari 0 akan melakukan perulangan
+			//dikurang sampai false
+		for(int d=1; d<c+1; d++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
